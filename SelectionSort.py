283b7c3eca657eda4cae686aa487edabc6dc0c93
Mohamed Shoaib <mohamedshoaib253@gmail.com>
2024-10-25 17:03:29 +0300

    Create SelectionSort.py

---
+++ SelectionSort.py
@@ -0,0 +1,11 @@
+def selectionSort(arr):
+    new_arr = []
+    for i in range(len(arr)):
+        smallest = arr[0]
+        smallest_index = 0
+        for j in range(1, len(arr)):
+            if arr[j] < smallest:
+                smallest = arr[j]
+                smallest_index = j
+        new_arr.append(arr.pop(smallest_index))  # استخدام smallest_index هنا
+    return new_arr
